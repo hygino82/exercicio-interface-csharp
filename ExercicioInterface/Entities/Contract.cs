6a89eee82c4d730ccf9c5ab08a09623f0b9283dd
Adroaldo <hygino@msn.com>
2021-03-02 08:54:42 -0300

    Solução sem o uso de interface

---
+++ ExercicioInterface/Entities/Contract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioInterface.Entities
+{
+    class Contract
+    {
+        public int Number { get; private set; }
+        public DateTime Date { get; private set; }
+        public double TotalValue { get; private set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+    }
+}
---
+++ ExercicioInterface/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using ExercicioInterface.Entities;
+
+namespace ExercicioInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = 8028;//int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date = DateTime.Parse("25/06/2018");//Console.ReadLine());
+            Console.Write("Contract value: ");
+            double totalValue = 600.0;//double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Contract contract = new Contract(number, date, totalValue);
+            List<Installment> list = new List<Installment>();
+
+
+            Console.Write("Enter number of installments: ");
+            int installments = 3;//int.Parse(Console.ReadLine());
+            double baseValue = totalValue / installments;
+            Console.WriteLine();
+
+            for (int i = 1; i <= installments; i++)
+            {
+                double amount = baseValue * (1 + i / 100.0);
+                amount *= 1.02;
+                DateTime dueDate = date.AddMonths(i);
+
+                Installment installment = new Installment(dueDate, amount);
+                list.Add(installment);
+            }
+
+            Console.WriteLine("Installments:");
+            foreach (Installment p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
